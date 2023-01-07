8170ba718f695e520120bc2c846ba2c159e3fc44
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-01-06 21:30:28 -0800

    StringsAndDates

--- StringsAndDates.py
+++ StringsAndDates.py
@@ -1,3 +1,9 @@
 #Justin Stevens CIS261 Lab 2
 
-enter_date - input("Please enter date as Month Day, Year:  ")
+enter_date = input("Please enter date as Month Day, Year:  ")
+while enter_date != '-1': 
+    tokens= enter_date.split()   
+    month= tokens[0]
+    if month == 'January':
+        month_int = 1
+    
